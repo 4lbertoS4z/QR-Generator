cc818f90064605ea0886d30019a3be9ed227c763
unknown <sniperalberto91@gmail.com>
2023-04-07 14:10:49 +0200

    Ahora el QR se guarda en la galeria, interfaz mejorada

--- app/src/main/java/com/example/qrgenerator/MainActivity.kt
+++ app/src/main/java/com/example/qrgenerator/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.qrgenerator
 
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import com.example.qrgenerator.databinding.ActivityMainBinding
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.qrcode.QRCodeWriter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 private lateinit var binding: ActivityMainBinding
 
@@ -18,38 +25,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener {
-            val url = binding.editText.text.toString()
-            if (url.isEmpty()) {
-                Toast.makeText(this, "Por favor ingrese una URL o texto", Toast.LENGTH_SHORT).show()
 
-            } else {
-                generateQRCode(url)
-            }
-
+        binding.buttonReadQr.setOnClickListener {
+            startActivity(Intent(this, QrScannerActivity::class.java))
         }
-        binding.buttonCamera.setOnClickListener {
-         startActivity(Intent(this, QrScannerActivity::class.java))
-
-        }
-    }
-
-    private fun generateQRCode(text: String) {
-
-        val writer = QRCodeWriter()
-        try {
-            val bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 512, 512)
-            val width = bitMatrix.width
-            val height = bitMatrix.height
-            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-            for (x in 0 until width) {
-                for (y in 0 until height) {
-                    bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
-                }
-            }
-            binding.imageView.setImageBitmap(bitmap)
-        } catch (e: WriterException) {
-            e.printStackTrace()
+        binding.buttonGenerateQr.setOnClickListener {
+            startActivity(Intent(this,QrGenerateImage::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/example/qrgenerator/QrGenerateImage.kt
@@ -0,0 +1,87 @@
+package com.example.qrgenerator
+
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import com.example.qrgenerator.databinding.ActivityMainBinding
+import com.example.qrgenerator.databinding.ActivityQrGenerateImageBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+import java.io.File
+import java.io.IOException
+
+private lateinit var binding: ActivityQrGenerateImageBinding
+class QrGenerateImage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQrGenerateImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            val url = binding.editText.text.toString()
+            if (url.isEmpty()) {
+                Toast.makeText(this, "Por favor ingrese una URL o texto", Toast.LENGTH_SHORT).show()
+
+            } else {
+                generateQRCode(url)
+            }
+
+        }
+
+    }
+
+    private fun generateQRCode(text: String) {
+
+        val writer = QRCodeWriter()
+        try {
+            val bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 512, 512)
+            val width = bitMatrix.width
+            val height = bitMatrix.height
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+                }
+            }
+            binding.imageView.setImageBitmap(bitmap)
+            //Con esto podremos guardar el Qr generado en la galeria de nuestro telefono movil
+            val values = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, "qr_code_${System.currentTimeMillis()}.jpg")
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + File.separator + "QR Codes")
+                    put(MediaStore.Images.Media.IS_PENDING, 1)
+                }
+            }
+
+            val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            uri?.let {
+                try {
+                    contentResolver.openOutputStream(uri)?.use { outputStream ->
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        values.clear()
+                        values.put(MediaStore.Images.Media.IS_PENDING, 0)
+                        contentResolver.update(uri, values, null, null)
+                    }
+                    Toast.makeText(this, "QR Code guardado en la galería", Toast.LENGTH_SHORT).show()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Error al guardar el QR Code en la galería", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        } catch (e: WriterException) {
+            e.printStackTrace()
+        }
+    }
+
+}
